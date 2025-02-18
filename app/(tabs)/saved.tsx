96ac4868597c46d7b86bb0ca7a78fc5c4b500ecc
sufi4414 <mdsufyanali@gmail.com>
2025-02-18 13:06:01 +0800

    Saved Page v1

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,9 +1,2 @@
-import { Text } from "@/components/Themed";
-import { Button, ButtonText } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
-
-export default function SavedList() {
-  return (
-    <Heading>Saved List</Heading>
-  );
-}
+import { SavedPage } from "@/screens/saved-page";
+export default SavedPage;
