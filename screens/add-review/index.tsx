70b0e0234f6edef27ed54f0bcff1442adec797d2
sufi4414 <mdsufyanali@gmail.com>
2025-03-14 07:07:25 +0800

    Image Picker

--- screens/add-review/index.tsx
+++ screens/add-review/index.tsx
@@ -1,12 +1,31 @@
 import { Button, ButtonText } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
 import { Input, InputField } from "@/components/ui/input";
 import { Slider, SliderFilledTrack, SliderThumb, SliderTrack } from "@/components/ui/slider";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+import { useState } from "react";
+import { TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as ImagePicker from "expo-image-picker";
 
 const AddReview = () => {
+    const [images, setImages] = useState<(string | null)[]>([null, null, null]); // Allow 3 images
+
+  const pickImage = async (index) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+        const newImages = [...images];
+        newImages[index] = result.assets[0].uri;
+        setImages(newImages);
+      }
+  };
   return (
     <VStack space="xl">
     <VStack>
@@ -17,6 +36,17 @@ const AddReview = () => {
     </VStack>
     <VStack>
          <Text>Did you take any photos? Add them here..</Text>
+         <HStack space="xl">
+            {images.map((image, index) => (
+                <TouchableOpacity key={index} onPress={() => pickImage(index)}>
+                {image ? (
+                    <Image source={{ uri: image }} style={{ width: 60, height: 60, borderRadius: 10 }} />
+                ) : (
+                    <Text style={{ fontSize: 24 }}>+</Text>
+                )}
+                </TouchableOpacity>
+            ))}
+         </HStack>
          <Input>
             <InputField placeholder="Write a caption to get your audience excited.."></InputField>
         </Input>
