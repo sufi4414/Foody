0534a00b542536b53cb18e5473a9f66a7b2912ae
sufi4414 <mdsufyanali@gmail.com>
2025-03-24 02:53:51 +0800

    Storefront

--- services/apiServices.ts
+++ services/apiServices.ts
@@ -1,7 +1,7 @@
 // services/apiService.ts
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const BASE_URL = "http://10.0.2.2:8000";
+export const BASE_URL = "http://192.168.1.223:8000";
 
 const getHeaders = async () => {
   const jwt = await AsyncStorage.getItem("jwt");
@@ -149,3 +149,81 @@ export const getReview = async (reviewId: string) => {
   }
   return response.json();
 }
+
+export const fetchEateryById = async (eatery_id) => {
+  const jwt = await AsyncStorage.getItem("jwt");
+  console.log("Connecting to DB..")
+  console.log("Fetching eatery by ID....")
+
+  const response = await fetch(`${BASE_URL}/eatery/get/${eatery_id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  if (!response.ok) {
+      throw new Error("Failed to fetch eateries");
+  }
+  return await response.json();
+};
+
+
+export const postSavedEatery = async (eateryId) => {
+  console.log("EateryID to be saved:", eateryId);
+  const jwt = await AsyncStorage.getItem("jwt");
+  if (!jwt) throw new Error("No JWT token found");
+
+  const response = await fetch(`${BASE_URL}/savelist/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",  
+      Authorization: `Bearer ${jwt}`,
+    },
+    body: JSON.stringify({ eatery_id: eateryId }), 
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error posting eatery data: ${response.status}`);
+  }
+
+  return await response.json();
+};
+
+export const deleteSavedEatery = async (eateryId: number) => {
+  const jwt = await AsyncStorage.getItem("jwt");
+  if (!jwt) throw new Error("No JWT token found");
+
+  const response = await fetch(`${BASE_URL}/savelist/${eateryId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error deleting saved eatery: ${response.status}`);
+  }
+
+  return await response.json();
+};
+
+export const fetchEateryFeed = async (eatery_id) => {
+  const jwt = await AsyncStorage.getItem("jwt");
+  console.log("Connecting to DB..")
+  console.log("Fetching eatery reviews....")
+
+  const response = await fetch(`${BASE_URL}/reviewpreviews/eatery/${eatery_id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  if (!response.ok) {
+      throw new Error("Failed to fetch eateries");
+  }
+  return await response.json();
+};
